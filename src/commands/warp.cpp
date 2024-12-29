031b2981b90c13e00d1315e54f7164fee0e518a7
Rosco994 <121896453+Rosco994@users.noreply.github.com>
2024-12-29 21:37:58 +0000

    Warp Aliases & GlobalWarp

--- src/commands/warp.cpp
+++ src/commands/warp.cpp
@@ -75,12 +75,56 @@ void WarpToMe(const std::vector<std::string>& arguments, Character* from)
 	}
 }
 
+void GlobalWarp(const std::vector<std::string>& arguments, Character* from)
+{
+     UTIL_FOREACH(from->world->characters, character)
+     {
+            if(character->mapid != static_cast<int>(from->world->config["JailMap"]))
+            {
+		// character->ServerMsg("The Admins has warped everyone for a special reason, listen for details." );
+		character->ServerMsg("Hey "+(util::ucfirst(character->SourceName())+", The Admins has warped everyone for a special reason, listen for details."));
+                character->Warp(from->mapid, from->x, from->y, WARP_ANIMATION_NONE);
+            }
+     }
+}
+
+void WarpPlayer(const std::vector<std::string>& arguments, Character* from)
+{
+    Character *victim = from->world->GetCharacter(arguments[0]);
+ 
+    int map = util::to_int(arguments[1]);
+    int x = util::to_int(arguments[2]);
+    int y = util::to_int(arguments[3]);
+ 
+    if(!victim)
+    {
+        from->ServerMsg("Unable to find character");
+    }
+    else
+    {
+        if (victim->admin < int(from->world->admin_config["cmdprotect"]) || victim->admin <= from->SourceAccess())
+        {
+            victim->Warp(map, x, y, from->world->config["WarpBubbles"] ? WARP_ANIMATION_ADMIN : WARP_ANIMATION_NONE);
+        }
+        else
+        {
+            from->ServerMsg("You tried to warp someone that is higher level admin than you.");
+        }
+    }
+}
+
 COMMAND_HANDLER_REGISTER(warp)
 	RegisterCharacter({"warp", {"map", "x", "y"}}, Warp, CMD_FLAG_DUTY_RESTRICT);
+	RegisterCharacter({"warpplayer", {"victim", "map", "x", "y"}}, WarpPlayer, CMD_FLAG_DUTY_RESTRICT);
 	RegisterCharacter({"warpmeto", {"victim"}}, WarpMeTo, CMD_FLAG_DUTY_RESTRICT);
 	RegisterCharacter({"warptome", {"victim"}}, WarpToMe, CMD_FLAG_DUTY_RESTRICT);
+	RegisterCharacter({"globalwarp"}, GlobalWarp, CMD_FLAG_DUTY_RESTRICT);
 
 	RegisterAlias("w", "warp");
+	RegisterAlias("wp", "warpplayer");
+        RegisterAlias("wtm", "warptome");
+        RegisterAlias("wmt", "warpmeto");
+	RegisterAlias("gw", "globalwarp");
 COMMAND_HANDLER_REGISTER_END(warp)
 
 }
